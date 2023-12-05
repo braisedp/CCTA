9950569ae8a5d7d47f57f18331190b0fed6816ae
braisedp <1938673948@qq.com>
2023-12-05 20:38:36 +0800

    gen_prb,read_graph added

--- graphUtils.py
+++ graphUtils.py
@@ -1,5 +1,8 @@
-import networkx
+import networkx as nx
 import random
+import time
+import re
+from copy import deepcopy
 
 
 def influence(G, S, Ep):
@@ -13,3 +16,34 @@ def influence(G, S, Ep):
                 else:
                     continue
     return F
+
+
+def gen_prb (n, mu, sigma, lower=0, upper=1):
+    '''Generate probability from normal distribution in the range [0,1].
+    '''
+    import scipy.stats as stats
+    X = stats.truncnorm(
+         (lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma)
+    return X.rvs(n)
+
+def read_graph(filename, directed=False):
+    if not directed:
+        G = nx.Graph()
+    else:
+        G = nx.DiGraph()
+    with open(filename) as f:
+        for line in f:
+            e0, e1 = map(int, line.split())
+            G.add_edge(e0, e1)
+    return G
+
+
+def generate_graph(filename, directed=False):
+    start = time.time()
+
+    G = read_graph(filename, directed=False)
+
+    print('reading graph')
+    print('time-used:{t:.2f}'.format(t=time.time() - start))
+
+
