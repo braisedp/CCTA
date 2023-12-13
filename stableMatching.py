3268626a97b348914b05dfd3bf1c0c3e6b0d2bce
braisedp <1938673948@qq.com>
2023-12-13 17:09:46 +0800

    correct problems

--- stableMatching.py
+++ stableMatching.py
@@ -3,9 +3,9 @@ from abc import ABCMeta, abstractmethod
 from typing import List
 
 
-class Hospital(metaclass=ABCMeta):
+class School(metaclass=ABCMeta):
     @abstractmethod
-    def choice(self, students: List) -> List:
+    def choice(self, student):
         pass
 
     @abstractmethod
@@ -13,22 +13,18 @@ class Hospital(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def capable(self, students: List) -> List:
+    def preview(self, students: List):
         pass
 
     @abstractmethod
     def students(self) -> List:
         pass
 
-    @abstractmethod
-    def matched(self) -> bool:
-        pass
-
 
 class Student(metaclass=ABCMeta):
 
     @abstractmethod
-    def init_preference_list(self, hospitals: List):
+    def preview(self, schools: List):
         pass
 
     @abstractmethod
@@ -36,15 +32,19 @@ class Student(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def propose(self) -> Hospital:
+    def propose(self) -> School:
         pass
 
     @abstractmethod
-    def rejected_by(self, hospital: Hospital):
+    def rejected_by(self, school: School):
         pass
 
     @abstractmethod
-    def hospital(self) -> Hospital:
+    def chosen_by(self, school: School):
+        pass
+
+    @abstractmethod
+    def school(self) -> School:
         pass
 
     @abstractmethod
@@ -52,38 +52,40 @@ class Student(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def prefer(self, hospital1, hospital2) -> bool:
+    def prefer(self, school) -> bool:
         pass
 
 
 def all_matched(students: List) -> bool:
     flag = True
     for s in students:
-        if not s.matched:
+        if not s.matched():
             flag = False
     return flag
 
 
 def choose(students: List) -> Student:
-    l = filter(lambda s: not s.matched, students)
+    l = list(filter(lambda s: not s.matched(), students))
     return random.choice(l)
 
 
-def generalized_da(hospitals: List, students: List):
+def generalized_da(schools: List, students: List):
     for student in students:
-        student.init_preference_list(hospitals)
+        student.preview(schools)
+    for school in schools:
+        school.preview(students)
     while not all_matched(students):
         student = choose(students)
-        h = student.propose()
-        h.choice(h.students() + [student])
-    return hospitals, students
+        school = student.propose()
+        school.choice(student)
+    return schools, students
 
 
-def heuristic(hospitals: List, students: List, k: int):
+def heuristic(schools: List, students: List, k: int):
     for i in range(k):
-        for hospital in hospitals:
-            s_h = hospital.students().copy()
+        for school in schools:
+            l = school.students().copy()
             for student in students:
-                if hospital in student.preference_list() and student.prefer(hospital, student.hospital()):
-                    s_h.append(student)
-            hospital.select(s_h)
+                if school in student.preference_list() and student.prefer(school):
+                    l.append(student)
+            school.select(l)
